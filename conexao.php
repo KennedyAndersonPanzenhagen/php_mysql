b5dd3b3e62da370f3294a299e2a5567e227f25d8
Kennedy Anderson <48600151+KennedyAndersonPanzenhagen@users.noreply.github.com>
2019-03-15 14:19:29 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,16 +1,14 @@
 <?php
-//$servidor = "localhost";
-//$usuario = "root";
-//$senha = "";
-//$dbname = "novo_usuario";
-//
-//if (mysqli_connect_errno()) {
-//    echo "Falha na conexão com o servido MySQL:  " . mysqli_connect_error();
-//} else {
-//    echo 'Conexão Ok!';
-//}
-//
-////cria a conexão
-//$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-//
-//
+$servidor = "seu servidor/localhost";
+$usuario = "seu usuario";
+$senha = "sua senha";
+$dbname = "nome do banco de dados";
+
+if (mysqli_connect_errno()) {
+    echo "Falha na conexão com o servido MySQL:  " . mysqli_connect_error();
+} else {
+    echo 'Conexão Ok!';
+}
+
+//cria a conexão
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
