67cafd21cc0fb002e8ebaa9b0a9487169f48f193
Kennedy Anderson <48600151+KennedyAndersonPanzenhagen@users.noreply.github.com>
2019-03-15 14:15:54 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<?php
+//$servidor = "localhost";
+//$usuario = "root";
+//$senha = "";
+//$dbname = "novo_usuario";
+//
+//if (mysqli_connect_errno()) {
+//    echo "Falha na conexão com o servido MySQL:  " . mysqli_connect_error();
+//} else {
+//    echo 'Conexão Ok!';
+//}
+//
+////cria a conexão
+//$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+//
+//
---
+++ processa.php
@@ -0,0 +1,29 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '', 'novo_usuario');
+
+if (mysqli_connect_errno()) {
+    die('Erro na conexão.');
+} else {
+    echo 'Conexão Ok!';
+}
+
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$data = date('Y-m-d H:i:s');
+$idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_STRING);
+$cidade = filter_input(INPUT_POST, 'cidade', FILTER_SANITIZE_STRING);
+$cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_STRING);
+var_dump($cpf);
+$mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_STRING);
+
+$insertQuery = "INSERT INTO usuario (nome, email, created, idade, cidade, cpf, mensagem ) VALUES ('$nome', '$email', '$data', '$idade', '$cidade', '$cpf', '$mensagem')";
+$resultado_usuario = mysqli_query($conn, $insertQuery);
+
+
+if($resultado_usuario === true){
+    echo"<script language='javascript' type='text/javascript'>alert('Cadastro realizado com sucesso!');window.location.href='siteform.html'</script>";
+}else{
+    die(mysqli_error($conn));
+    echo"<script language='javascript' type='text/javascript'>alert('Houve um erro inesperado! ');window.location.href='siteform.html'</script>";
+}
